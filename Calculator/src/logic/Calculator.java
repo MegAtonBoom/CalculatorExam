218812f276a1fada735443c581d8bb67e1080908
Adrian Baba <40306263+MegAtonBoom@users.noreply.github.com>
2021-02-18 09:36:16 +0100

    1

---
+++ Calculator/src/logic/Calculator.java
@@ -0,0 +1,31 @@
+package logic;
+
+
+
+public class Calculator {
+	
+	public double add(double a, double b) {
+		return a+b;
+	}
+	
+	public double sub(double a, double b) {
+		return a-b;
+	}
+	
+	public double mult(double a, double b) {
+		return a*b;
+	}
+	
+	public double div(double a, double b) {
+		return a/b;
+	}
+	
+	public double logarithm(double a) {
+		return Math.log(a);
+	}
+	
+	public double sqroot(double a) {
+		return Math.sqrt(a);
+	}
+
+}
